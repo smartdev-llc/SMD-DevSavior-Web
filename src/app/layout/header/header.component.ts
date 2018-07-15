bc1a317bc108416c333065a408f24e13ca815560
Thu Nguyen <thu.nguyen@smartdev.vn>
2018-07-15 19:16:52 +0700

    create home-header component

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,15 +1,4 @@
-import { environment } from './../../../environments/environment';
-import { Router } from '@angular/router';
-import { SearchActions } from './../../home/reducers/search.actions';
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { Store } from '@ngrx/store';
-import { AppState } from '../../interfaces';
-import { getAuthStatus } from '../../auth/reducers/selectors';
-import { Observable } from 'rxjs';
-import { AuthService } from '../../core/services/auth.service';
-import { AuthActions } from '../../auth/actions/auth.actions';
-import { TemplateRef } from '@angular/core';
-import { Directive, Renderer2, ElementRef } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -18,21 +7,9 @@ import { Directive, Renderer2, ElementRef } from '@angular/core';
 })
 
 export class HeaderComponent implements OnInit {
-  constructor(
-    private store: Store<AppState>,
-    private authService: AuthService,
-    private authActions: AuthActions,
-    private searchActions: SearchActions,
-    private router: Router, private renderer: Renderer2) {
-  }
-
-  openModalWithClass(template: TemplateRef<any>) {
-    this.renderer.addClass(document.body, 'cat-mobile-open');
-
+  constructor() {
   }
 
   ngOnInit() {
-    this.store.dispatch(this.authActions.authorize());
-    this.store.dispatch(this.authActions.login());
   }
 }
