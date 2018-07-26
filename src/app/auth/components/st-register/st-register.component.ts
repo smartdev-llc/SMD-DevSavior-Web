a3ca0b16f311fbcd4dc65bd3410f4c34532332c0
Thu Nguyen <npanhthu.it@gmail.com>
2018-07-26 20:22:40 +0700

    create login and register component

---
+++ src/app/auth/components/st-register/st-register.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'job-detail',
+  templateUrl: './st-register.component.html',
+  styleUrls: ['./st-register.component.scss']
+})
+export class StRegisterComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+}
