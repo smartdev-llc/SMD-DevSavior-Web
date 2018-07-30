a8c185c95c79ea5ccc7c78f0db4a0b4250af23bd
Tai Huynh <tai.huynh@mgm-tp.com>
2018-07-30 20:45:18 +0700

    FE-4.2: organizing imports

--- src/app/home/index.ts
+++ src/app/home/index.ts
@@ -3,11 +3,10 @@ import { NgModule } from '@angular/core';
 import { NgxInputStarRatingModule } from '@ngx-lite/input-star-rating';
 
 import { LayoutModule } from '../layout/index';
+import { SharedModule } from '../shared/shared.module';
 // Components
 import { HomeComponent } from './home.component';
 import  { JobsFiltersComponent } from'./jobs-filters/jobs-filters.component';
-import { SharedModule } from '../shared/shared.module';
-// Components
 // Breadcrumb components
 // Routes
 import { HomeRoutes as routes } from './home.routes';
