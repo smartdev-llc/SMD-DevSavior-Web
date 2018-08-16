aad372a8c0e081b43d43a416887685ccb9ec3c3d
hongvan <hongvant2@gmail.com>
2018-08-17 00:50:06 +0700

    Changed API route domain

--- src/config/default.ts
+++ src/config/default.ts
@@ -1,6 +1,6 @@
 export const DEFAULT_CONFIG = {
   // Add Your custom configs here
-  prodApiEndpoint: 'https://164.132.226.137:1337',
+  prodApiEndpoint: 'https://juniorviec.com:1337',
   appName: 'JuniorViec',
   fevicon: 'http://via.placeholder.com/350x150',
   header: {
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ import { DEFAULT_CONFIG } from '../config/default';
 
 export const environment = {
   production: true,
-  apiEndpoint: 'https://164.132.226.137:1337',
+  apiEndpoint: 'https://juniorviec.com:1337',
   appName: DEFAULT_CONFIG.appName,
   config: DEFAULT_CONFIG
 };
