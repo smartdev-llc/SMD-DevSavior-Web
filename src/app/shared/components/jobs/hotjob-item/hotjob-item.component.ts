4aeb18c89fe6f54834270813cf5b9516e850676a
Vunguyen <vunv.uk@gmail.com>
2018-08-24 17:48:13 +0700

    [#70][home-page] display hot jobs

---
+++ src/app/shared/components/jobs/hotjob-item/hotjob-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'hotjob-item',
+  templateUrl: './hotjob-item.component.html',
+  styleUrls: ['./hotjob-item.component.scss']
+})
+export class HotJobItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
