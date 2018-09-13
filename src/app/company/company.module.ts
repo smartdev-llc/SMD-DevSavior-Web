e7885418b3965eb4ee4cd4d85a09ebf0439b4d9e
Tai Huynh <tai.huynh@mgm-tp.com>
2018-09-13 20:02:27 +0700

    init structure company module

---
+++ src/app/company/company.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponentComponent } from './home-component/home-component.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [HomeComponentComponent]
+})
+export class CompanyModule { }
