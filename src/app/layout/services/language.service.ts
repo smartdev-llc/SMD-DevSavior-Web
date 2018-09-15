88f78953192d8ab387cd52b3d0861df6ae14d93d
Thu Nguyen <thu.nguyen@smartdev.vn>
2018-09-15 20:01:56 +0700

    add language service

---
+++ src/app/layout/services/language.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { TranslateCacheService } from 'ngx-translate-cache';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class LanguageService {
+
+  private DEFAULT_LANG = 'en';
+
+  constructor(
+    private translateService: TranslateService,
+    private translateCacheService: TranslateCacheService
+  ) {
+    translateCacheService.init();
+    this.translateService.setDefaultLang(this.DEFAULT_LANG);
+  }
+
+  setDefaultLang() {
+    const lang = this.translateCacheService.getCachedLanguage() || this.DEFAULT_LANG;
+    this.translateService.use(lang);
+  }
+
+  getCachedLanguage() {
+    return this.translateCacheService.getCachedLanguage() || this.DEFAULT_LANG;
+  }
+
+  changeLanguage(language: string): Observable<any> {
+    return this.translateService.use(language)
+  }
+}
