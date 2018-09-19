dd19bb8403ca61c3af92bbcc347116d4f7c14a44
Vunguyen <vunv.uk@gmail.com>
2018-09-19 16:20:22 +0700

    [#98][company]Find resumes candidate - screen 4

--- src/app/company/components/find-resumes/find-resumes.component.ts
+++ src/app/company/components/find-resumes/find-resumes.component.ts
@@ -6,21 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./find-resumes.component.scss']
 })
 export class FindResumesComponent implements OnInit {
-  
+
   users = [
-    {id: '1', name: 'Graduated'},
-    {id: '2', name: 'Employee'},
-    {id: '3', name: 'Leader'},
-    {id: '4', name: 'Manager'},
-    {id: '5', name: 'Direction'}
-];
+    { id: '1', name: 'Graduated' },
+    { id: '2', name: 'Employee' },
+    { id: '3', name: 'Leader' },
+    { id: '4', name: 'Manager' },
+    { id: '5', name: 'Direction' }
+  ];
 
-selectedUserIds: number[];
-today: number = Date.now();
-model;
-constructor() {}
+  selectedUserIds: number[];
+  model;
+  constructor() { }
 
-ngOnInit(){}
+  ngOnInit() { }
 
-addCustomUser = (term) => ({id: term, name: term});
+  addCustomUser = (term) => ({ id: term, name: term });
 }
