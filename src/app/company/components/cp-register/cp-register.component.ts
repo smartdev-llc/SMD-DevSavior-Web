c593114fbc55e96b38e3dcacf23f507ef73de959
quangtuanhv <quangtuanhv@gmail.com>
2018-10-05 17:23:47 +0700

    API integration Register

--- src/app/company/components/cp-register/cp-register.component.ts
+++ src/app/company/components/cp-register/cp-register.component.ts
@@ -1,15 +1,107 @@
 import { Component, OnInit } from '@angular/core';
+import {  FormBuilder,  FormGroup,  Validators } from '@angular/forms';
+import { UserCompany,  Role } from '../../../core/models/user';
+import { matchingPasswordValidator } from '../../../auth/validators/matching-password.directive';
+import { AuthService } from '../../../core/services/auth.service';
+import { Router, NavigationExtras } from '@angular/router';
 
 @Component({
   selector: 'app-cp-register',
   templateUrl: './cp-register.component.html',
-  styleUrls: ['./cp-register.component.css']
+  styleUrls: ['./cp-register.component.css'],
+  providers: [ AuthService ]
 })
 export class CpRegisterComponent implements OnInit {
+  static MIN_LENGTH_PASSWORD = 8;
+  static DEFAULT_MESSAGE = 'Oops! Something bad happened. Please come back later.';
+  registerForm: FormGroup;
+  passwordGroup: FormGroup;
+  submitted: boolean;
+  loading: boolean;
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private formBuilder: FormBuilder) { }
+  registerNewUser() {
+    this.loading = true;
+    this.submitted = true;
+      if (this.registerForm.invalid) {
+        this.loading = false;
+        return;
+      }
+      this.authService.register(this.convertToCompany())
+      .subscribe(
+        data => {
+          const navigationExtras: NavigationExtras = {
+          queryParams: { 'email': data.email },
+          skipLocationChange: true
+        };
+        this.router.navigate(['/register-success'], navigationExtras);
+        },
+        error=>{
+          this.resetPasswordForm();
+          if(error.status === 409){
+            this.f.email.setErrors({'existed':true});
+          }else if(error.status >= 500){
+            this.registerForm.setErrors({serverError: CpRegisterComponent.DEFAULT_MESSAGE});
+          }else{
+            this.registerForm.setErrors({serverError:error.error.message})
+          }
+          this.loading = false;
+        });
 
-  constructor() { }
-
+    }
+    resetPasswordForm() {
+      this.passwordGroup.reset();
+    }
+    get f() {
+      return this.registerForm.controls;
+    }
+  
+    get p() {
+      return this.passwordGroup.controls;
+    }
+  
+    get LETTER_ONLY_PATTERN() {
+      return '^[^@!~#$%^&*()}{]*$';
+    }
   ngOnInit() {
+    this.passwordGroup = this.formBuilder.group({
+      password:['',[Validators.required,Validators.minLength(CpRegisterComponent.MIN_LENGTH_PASSWORD)]],
+      repeatPassword:['',[Validators.required]]
+    },{
+      validators: matchingPasswordValidator
+    });
+    this.registerForm = this.formBuilder.group({
+      email:['', [Validators.required,Validators.email]],
+      passwordGroup:this.passwordGroup,
+      address:['',[Validators.required]],
+      city:['',[Validators.required]],
+      contactName:['',[Validators.required,Validators.pattern(this.LETTER_ONLY_PATTERN)]],
+      name:['',[Validators.required,Validators.pattern(this.LETTER_ONLY_PATTERN)]],
+      phoneNumber:['',[Validators.required]],
+      website:['',[Validators.required]],
+      role:[Role.Company],
+    });
+    this.loading = false ;
+
+  }
+  
+convertToCompany(): UserCompany {
+    const userCompany = new UserCompany();
+    userCompany.email = this.registerForm.value.email;
+    userCompany.password = this.passwordGroup.value.password;
+    userCompany.address = this.registerForm.value.address || '';
+    userCompany.city = this.registerForm.value.city || '';
+    userCompany.contactName = this.registerForm.value.contactName || '';
+    userCompany.name = this.registerForm.value.name || ''; 
+    userCompany.phoneNumber = this.registerForm.value.phoneNumber || '';
+    userCompany.website = this.registerForm.value.website || '';
+    userCompany.role = Role.Company;
+    return userCompany;
   }
 
+  get hasServerError() {
+    return this.submitted && this.registerForm.errors != null && this.registerForm.errors.serverError != null;
+  }
 }
