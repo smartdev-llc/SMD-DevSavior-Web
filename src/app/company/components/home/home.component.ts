577b6431f8f5317326ec40130ab233b7ed9e7462
Vunguyen <vunv.uk@gmail.com>
2018-10-10 19:44:42 +0700

    make carousel for company screen not login

--- src/app/company/components/home/home.component.ts
+++ src/app/company/components/home/home.component.ts
@@ -3,13 +3,24 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  slideChangeMessage = '';
+ 
+  slides = [
+    {image: '../../../../assets/images/slide01.jpg'},
+    {image: '../../../../assets/images/slide02.jpg'},
+    {image: '../../../../assets/images/slide03.jpg'},
+  ];
 
   constructor() { }
 
   ngOnInit() {
   }
-
+ 
+ 
+  log(event: number) {
+    this.slideChangeMessage = `Slide has been switched: ${event}`;
+  }
 }
---
+++ src/app/company/components/statistic/statistic.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '../../../../../node_modules/@angular/router';
+
+@Component({
+  selector: 'app-statistic',
+  templateUrl: './statistic.component.html',
+  styleUrls: ['./statistic.component.scss']
+})
+export class StatisticComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    
+  }
+
+}
