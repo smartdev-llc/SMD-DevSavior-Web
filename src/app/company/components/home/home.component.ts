aee9d1be3f82baab5bff4e664006300f23efeb87
Vunguyen <vunv.uk@gmail.com>
2018-10-12 17:47:27 +0700

    animation with angular

--- src/app/company/components/home/home.component.ts
+++ src/app/company/components/home/home.component.ts
@@ -1,9 +1,41 @@
 import { Component, OnInit } from '@angular/core';
-
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  keyframes
+} from '@angular/animations';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [
+    trigger('moveInLeft', [
+      state('left', style({ transform: 'translateX(-100px)', opacity: 0 })),
+      state('right', style({ transform: 'translate(0)', opacity: 1 })),
+      transition('left => right', animate('1s ease-out'))
+    ]),
+
+    trigger('moveInRight', [
+      state('right', style({ transform: 'translateX(100px)', opacity: 0 })),
+      state('left', style({ transform: 'translate(0)', opacity: 1 })),
+      transition('right => left', animate('3s ease-out'))
+    ]),
+
+    trigger('moveInBottom', [
+      state('bottom', style({ transform: 'translateY(100px)', opacity: 0 })),
+      state('top', style({ transform: 'translate(0)', opacity: 1 })),
+      transition('bottom => top', animate('3s ease-out'))
+    ]),
+
+    trigger('moveInTop', [
+      state('top', style({ transform: 'translateY(-100px)', opacity: 0 })),
+      state('bottom', style({ transform: 'translate(0)', opacity: 1 })),
+      transition('bottom => top', animate('3s ease-out'))
+    ]),
+  ]
 })
 export class HomeComponent implements OnInit {
   slides = [
@@ -13,17 +45,17 @@ export class HomeComponent implements OnInit {
       bigTitle: 'WITH MARKET\nTRENDS',
       buttonTitle: 'SUBCTIBE TO NEWSLETTER'
     },
-    { 
-      image: '../../../../assets/images/slide02.jpg', 
-      mainTitle: 'Stuck in a ‘career rut’?', 
+    {
+      image: '../../../../assets/images/slide02.jpg',
+      mainTitle: 'Stuck in a ‘career rut’?',
       bigTitle: 'HELP US MATCH TO\nYOUR HR  ROLE',
       buttonTitle: 'SUBCTIBE TO NEWSLETTER'
     },
-    { 
-      image: '../../../../assets/images/slide03.jpg', 
-      mainTitle: 'Launch Your recruitment career', 
+    {
+      image: '../../../../assets/images/slide03.jpg',
+      mainTitle: 'Launch Your recruitment career',
       bigTitle: 'WITH INNOVATE\nСONSULTANCY',
-      buttonTitle: 'JOHN OUR HR' 
+      buttonTitle: 'JOHN OUR HR'
     }
   ];
 
@@ -35,5 +67,5 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
   }
 
- 
+
 }
