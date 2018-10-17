b22cd6c22274a94b1c77eb3331764351aa4d5063
Thu Nguyen <thu.nguyen@smartdev.vn>
2018-10-17 11:29:12 +0700

    change header menu after login

--- src/app/layout/home-company/home-company.component.ts
+++ src/app/layout/home-company/home-company.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { TranslateCacheService } from 'ngx-translate-cache';
 import { LanguageService } from '../services/language.service';
+import { AuthService } from '../../core/services/auth.service';
+import { Subscription } from "rxjs";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home-company',
@@ -10,10 +13,14 @@ import { LanguageService } from '../services/language.service';
 })
 
 export class HomeCompanyComponent implements OnInit {
-  isEnLang: boolean = false
+  isEnLang: boolean = false;
+  user: any;
+  subscription: Subscription;
 
   constructor(
-    private languageService: LanguageService
+    private languageService: LanguageService,
+    private authService: AuthService,
+    private router: Router,
   ) {
     const lang = languageService.getCachedLanguage()
     languageService.setDefaultLang()
@@ -23,11 +30,37 @@ export class HomeCompanyComponent implements OnInit {
   }
 
   ngOnInit() {
+    let currentUser = this.authService.getCurrentUser();
+    if (!currentUser) {
+      this.subscription = this.authService.getLoggedInUser.subscribe((user: Object) => {
+        if (user) {
+          this.user = user;
+        }
+      })
+    } else {
+      this.user = currentUser;
+    }
   }
 
   changeLanguage(language: string): void {
     this.languageService.changeLanguage(language).subscribe(() => {
       language === 'en' ? this.isEnLang = true : this.isEnLang = false;
     });
+  }
+
+  logout() {
+    this.authService.logout()
+      .subscribe(_ => {
+        this.authService.signOut();
+        localStorage.removeItem('user');
+        this.router.navigate(['/employer/home']);
+        this.user = null;
+      })
+  }
+
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
   }
 }
