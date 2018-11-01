3bcec84e9caceced22a7abed391a25ba140963cc
Vunguyen <vunv.uk@gmail.com>
2018-11-01 16:53:06 +0700

    hot fix about-ú

--- src/app/auth/components/about-us/about-us.component.ts
+++ src/app/auth/components/about-us/about-us.component.ts
@@ -12,91 +12,91 @@ export class AboutUsComponent implements OnInit {
       avatar: '../../../../assets/images/nguyen.jpg',
       linkFB: 'https://www.facebook.com/le.v.nguyen.18',
       decsription: 'Founder and Sales Manager JuniorViec',
-      position:' Sales Manager'
+      position: ' Sales Manager'
     },
     {
       name: 'Tran Tien Dung',
       avatar: '../../../../assets/images/dung.jpg',
       linkFB: 'https://www.facebook.com/ttdung11t2',
-      decsription: 'Web Developer of JuniorViec',
-      position:'Web Developer'
+      decsription: 'Full-stack Developer of JuniorViec',
+      position: 'Full-stack Developer'
     },
     {
       name: 'Nguyen Phuoc Anh THu',
       avatar: '../../../../assets/images/thu.jpg',
       linkFB: 'https://www.facebook.com/anhthu.nguyenphuoc',
-      decsription: 'Web Developer of JuniorViec',
-      position:'Web Developer'
+      decsription: 'Full-stack Developer of JuniorViec',
+      position: 'Full-stack Developer'
     },
     {
       name: 'Huynh Phi Thien Tai',
       avatar: '../../../../assets/images/tai.jpg',
       linkFB: 'https://www.facebook.com/aibietcaij',
-      decsription: 'Web Developer of JuniorViec',
-      position:'Web Developer'
+      decsription: 'Full-stack Developer of JuniorViec',
+      position: 'Full-stack Developer'
     },
     {
       name: 'Vo Van Chuong',
       avatar: '../../../../assets/images/chuong.png',
       linkFB: '',
-      decsription: 'Web Developer of JuniorViec',
-      position:'Web Developer'
+      decsription: 'Full-stack Developer of JuniorViec',
+      position: 'Full-stack Developer'
     },
     {
       name: 'Vo Hong Van',
       avatar: '../../../../assets/images/van.jpg',
       linkFB: 'https://www.facebook.com/vonguu',
-      decsription: 'Web Developer of JuniorViec',
-      position:'Web Developer'
+      decsription: 'Full-stack Developer of JuniorViec',
+      position: 'Full-stack Developer'
     },
     {
       name: 'Phan Hoang Long',
       avatar: '../../../../assets/images/long.jpg',
       linkFB: 'https://www.facebook.com/long.lch',
-      decsription: 'Web Developer of JuniorViec',
-      position:'Web Developer'
+      decsription: 'Full-stack Developer of JuniorViec',
+      position: 'Full-stack Developer'
     },
     {
       name: 'Le Khanh Duy',
       avatar: '../../../../assets/images/duy.jpg',
       linkFB: 'https://www.facebook.com/khanhduy.179',
       decsription: 'Web Developer of JuniorViec',
-      position:'Web Developer'
+      position: 'Web Developer'
     },
     {
       name: 'Vo Quang Tuan',
       avatar: '../../../../assets/images/tuan.jpg',
       linkFB: 'https://www.facebook.com/quangtuandev',
-      decsription: 'Web Developer of JuniorViec',
-      position:'Web Developer'
+      decsription: 'Full-stack Developer of JuniorViec',
+      position: 'Full-stack Developer'
     },
     {
       name: 'Pham Hong Phuc',
       avatar: '../../../../assets/images/phuc.jpg',
       linkFB: 'https://www.facebook.com/hongphucpham97',
       decsription: 'QA of JuniorViec',
-      position:'QA'
+      position: 'QA'
     },
     {
       name: 'Vo Ngoc Anh',
       avatar: '../../../../assets/images/anh.jpg',
       linkFB: 'https://www.facebook.com/ngocanh.le.1029',
       decsription: 'QC of JuniorViec',
-      position:'QC'
+      position: 'QC'
     },
     {
       name: 'Le Nguyen Thanh Tuyen',
       avatar: '../../../../assets/images/tuyen.jpg',
       linkFB: 'https://www.facebook.com/dau.eyo',
       decsription: 'QC of JuniorViec',
-      position:'QC'
+      position: 'QC'
     },
-    
+
   ];
   constructor() { }
 
   ngOnInit() {
-    
+
   }
 
 }
