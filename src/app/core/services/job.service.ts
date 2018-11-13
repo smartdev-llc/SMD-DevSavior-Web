e9983ffb73d6a1af3c4ea284008db6f03daf8ebf
hongvan <hongvant2@gmail.com>
2018-11-13 20:58:39 +0700

    Removed consolog

--- src/app/core/services/job.service.ts
+++ src/app/core/services/job.service.ts
@@ -76,7 +76,6 @@ export class JobService {
       return throwError (new Duplicate(error.error.message));
     }
     if ( error.status === 404) {
-      console.log(error);
       return throwError (new NotFound(error.error.message));
     }
     return throwError(new AppErrors(error.error.message));
