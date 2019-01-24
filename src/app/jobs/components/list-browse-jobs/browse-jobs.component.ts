97a42fd6357c7f464be6f4a93d66b16ff2069e2a
Vu Nguyen <vunguyen@Smartdevs-iMac.local>
2019-01-24 17:58:04 +0700

    not done change background

--- src/app/jobs/components/list-browse-jobs/browse-jobs.component.ts
+++ src/app/jobs/components/list-browse-jobs/browse-jobs.component.ts
@@ -38,6 +38,14 @@ export class BrowseJobsComponent implements OnInit {
     { name: 'FREELANCE', id: 'FREELANCE' }
   ];
 
+  items: any = [
+    {bgcolor: '#967D69'},
+    {bgcolor: '#FFE381'},
+    {bgcolor: '#53A2BE'},
+    {bgcolor: '#157145'},
+    {bgcolor: '#57A773'}
+  ]
+
   constructor(
     private router: Router,
     private route: ActivatedRoute,
