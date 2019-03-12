1dec5f35b8bd56ed2185f523415dae073237b167
Vu Nguyen <vunguyen@vunguyen.local>
2019-03-12 18:43:56 +0700

    not yet working

--- src/app/auth/components/st-change-password/st-change-password.components.ts
+++ src/app/auth/components/st-change-password/st-change-password.components.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { TranslateService, LangChangeEvent } from '@ngx-translate/core';
 
 @Component({
   selector: 'st-change-password',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./st-change-password.components.scss']
 })
 export class StChangePasssword implements OnInit {
+  changePasswordFormGroup: FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private translate: TranslateService,
+    ) { }
 
   ngOnInit() {
+    this.initForms();
   }
 
+  initForms():void{
+    this.changePasswordFormGroup = this.formBuilder.group({
+      oldPassword: ['', Validators.required],
+      newPassword: ['', Validators.required]
+    })
+  }
 }
